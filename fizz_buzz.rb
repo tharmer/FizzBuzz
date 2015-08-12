9c83e1d55b3d109f2500211a54e542736d0f9bc4
Terry Harmer <terry.harmer@gmail.com>
2015-08-12 19:45:29 -0400

    first commit

---
+++ fizz_buzz.rb
@@ -0,0 +1,16 @@
+
+    puts "Enter a number to FizzBuzz"
+    i = gets
+    1.upto(i.to_i) do |i|
+      if (i % 5 == 0) && (i % 3 == 0)
+          puts 'FizzBuzz'
+      elsif
+          (i % 3 == 0)
+          puts "Fizz"
+      elsif
+          (i % 5 == 0)
+          puts "Buzz"
+      else
+          puts i
+      end
+    end
